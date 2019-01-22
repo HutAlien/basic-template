cd3a79732674d5a7e055c5e4c6684b983be67e36
Alien <1325130878@qq.com>
2019-01-22 16:29:56 +0800

    aop

---
+++ src/main/java/com/alien/basicTemplate/aspect/PermsAspect.java
@@ -0,0 +1,9 @@
+package com.alien.basicTemplate.aspect;
+
+/**
+ * @Auther: FengYunJun
+ * @Date: 2019/1/22 14:46
+ * @Description:
+ */
+public class Perms {
+}
---
+++ src/main/java/com/alien/basicTemplate/model/annotation/Perms.java
@@ -0,0 +1,9 @@
+package com.alien.basicTemplate.model.annotation;
+
+/**
+ * @Auther: FengYunJun
+ * @Date: 2019/1/22 14:53
+ * @Description:
+ */
+public @interface Perms {
+}
